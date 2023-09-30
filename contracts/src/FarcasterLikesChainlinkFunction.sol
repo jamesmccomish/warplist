8b0d3bca4894d0e0ee07c41010346c6b9b9c3184
jamesmccomish <james.mcc94@hotmail.co.uk>
2023-09-30 23:24:45 +0200

    wip: update nft & CL mint functions

--- contracts/src/FarcasterLikesChainlinkFunction.sol
+++ contracts/src/FarcasterLikesChainlinkFunction.sol
@@ -1,14 +1,45 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {FunctionsClient} from "../node_modules/@chainlink/contracts/src/v0.8/functions/dev/v1_0_0/FunctionsClient.sol";
+import {
+    FunctionsClient,
+    Functions
+} from "../node_modules/@chainlink/contracts/src/v0.8/functions/dev/v0_0_0/FunctionsClient.sol";
 
 // TODO
 // - link to chainlink
 // - add handler for allowing mints
 
+/**
+ * @title The Chainlink Functions client contract
+ * @notice Calls a functions to check wither a user has liked a Farcaster post
+ * @author James McComish (inspired by chainlink-functions-demo-app)
+ */
 contract FarcasterLikesChainlinkFunction is FunctionsClient {
-    constructor(address router) FunctionsClient(router) {}
+    uint64 public subscriptionId;
+
+    bytes32 public latestRequestId;
+    bytes public latestResponse;
+    bytes public latestError;
+
+    // We store the stringified version of the .js function to be executed
+    string farcasterApiCallLogic;
+
+    constructor(address router, string memory _farcasterApiCallLogic) FunctionsClient(router) {
+        farcasterApiCallLogic = _farcasterApiCallLogic;
+    }
+
+    /// -----------------------------------------------------------------------
+    /// UNSAFE OWNER FUNCTIONS (not restricted for demo purposes)
+    /// -----------------------------------------------------------------------
+
+    function setSubscriptionId(uint64 _subscriptionId) external {
+        subscriptionId = _subscriptionId;
+    }
+
+    /// -----------------------------------------------------------------------
+    /// CHAINLINK FUNCTIONS
+    /// -----------------------------------------------------------------------
 
     // function handleOracleFulfillment(bytes32 requestId, bytes memory response, bytes memory err) external override {
     //     if (msg.sender != address(i_router)) {
@@ -18,6 +49,8 @@ contract FarcasterLikesChainlinkFunction is FunctionsClient {
     // }
 
     function fulfillRequest(bytes32 requestId, bytes memory response, bytes memory err) internal override {
-        // TODO
+        latestResponse = response;
+        latestError = err;
+        //emit OCRResponse(requestId, response, err);
     }
 }
--- contracts/src/FarcasterRestrictedNft.sol
+++ contracts/src/FarcasterRestrictedNft.sol
@@ -3,13 +3,39 @@ pragma solidity ^0.8.19;
 
 import {ERC721} from "../lib/solbase/src/tokens/ERC721/ERC721.sol";
 
-import {FarcasterLikesChainlinkFunction} from "./FarcasterLikesChainlinkFunction.sol";
+import {FarcasterLikesChainlinkFunction, Functions} from "./FarcasterLikesChainlinkFunction.sol";
 
-contract FarcasterRestrictedNft is ERC721 {
-    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
+contract FarcasterRestrictedNft is ERC721, FarcasterLikesChainlinkFunction {
+    using Functions for Functions.Request;
 
-    function mint(address to, uint256 id) external {
-        _mint(to, id);
+    uint256 public currentId;
+
+    constructor(string memory _name, string memory _symbol, address _router, string memory _farcasterApiCallLogic)
+        ERC721(_name, _symbol)
+        FarcasterLikesChainlinkFunction(_router, _farcasterApiCallLogic)
+    {}
+
+    /// -----------------------------------------------------------------------
+    /// ERC721 FUNCTIONS
+    /// -----------------------------------------------------------------------
+    function whitelistMint() external {
+        // TODO here we should call the chainlink fn to check if the user is allowed to mint (using msg.sender as an arg)
+
+        Functions.Request memory req;
+        req.initializeRequest(Functions.Location.Inline, Functions.CodeLanguage.JavaScript, farcasterApiCallLogic);
+        //req.addArgs(args);
+
+        bytes32 assignedReqID = sendRequest(req, subscriptionId, type(uint32).max); // TODO fix gasLimit
+        latestRequestId = assignedReqID;
+        //return assignedReqID;
+
+        // _mint(to, id);
+    }
+
+    // Maintain interface
+    function mint(address to) external {
+        // _mint(to, currentId);
+        // currentId++;
     }
 
     function burn(uint256 id) external {
@@ -18,5 +44,28 @@ contract FarcasterRestrictedNft is ERC721 {
 
     function tokenURI(uint256 id) public view override returns (string memory) {
         return "TODO";
+    }
+
+    /// -----------------------------------------------------------------------
+    /// ERC721 FUNCTIONS
+    /// -----------------------------------------------------------------------
+
+    /**
+     * @notice Callback that is invoked once the DON has resolved the request or hit an error
+     *
+     * @param requestId The request ID, returned by sendRequest()
+     * @param response Aggregated response from the user code
+     * @param err Aggregated error from the user code or from the execution pipeline
+     * Either response or error parameter will be set, but never both
+     */
+    function fulfillRequest(bytes32 requestId, bytes memory response, bytes memory err) internal override {
+        super.fulfillRequest(requestId, response, err);
+
+        // TODO
+        // finalise the mint given a correct response
+
+        (address to, uint256 id) = abi.decode(response, (address, uint256));
+
+        _mint(to, id);
     }
 }

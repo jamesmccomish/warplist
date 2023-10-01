c9011be9d67f6f7148a7446f3f70031ca63c1d8b
jamesmccomish <james.mcc94@hotmail.co.uk>
2023-10-01 10:37:02 +0200

    wip: add frontend

---
+++ frontend/utils/get-viem-client.ts
@@ -0,0 +1,9 @@
+import { createPublicClient, http } from 'viem'
+import { mainnet } from 'viem/chains'
+
+export const publicClient = createPublicClient({
+  chain: mainnet,
+  transport: http()
+})
+
+ 

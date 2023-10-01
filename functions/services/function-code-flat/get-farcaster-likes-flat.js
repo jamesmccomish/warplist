d66f667c957457e583050a48b989171a9407e9d5
jamesmccomish <james.mcc94@hotmail.co.uk>
2023-10-01 04:25:40 +0200

    chore: setup example code

--- functions/services/function-code-flat/get-farcaster-likes-flat.js
+++ functions/services/function-code-flat/get-farcaster-likes-flat.js
@@ -2,13 +2,14 @@
 const baseUrl = 'https://api.warpcast.com';
 const version = 'v2';
 
-const callerAddress = args[0];
+/// @dev uncomment for production
+//const callerAddress = args[0];
 
 const warpcaster = async (path) => {
   const url = `${baseUrl}/${version}${path}`;
 
   try {
-    const response = await Functions.makeHttpRequest(url, {
+    const response = await fetch(url, {
       headers: {
         'Content-Type': 'application/json', // Adjust headers as needed
         // Add any other headers here
@@ -29,10 +30,9 @@ const warpcaster = async (path) => {
   }
 };
 
-const getFarcasterLikes = async (castHash) => {
+const getFarcasterLikes = async (castHash, callerAddress) => {
   try {
-    console.log('getFarcasterLikes fn ---');
-    const res = await warpcaster(`/cast-reactions?castHash=${castHash}&limit=10`);
+    const res = await warpcaster(`/cast-reactions?castHash=${castHash}&limit=100`);
 
     const reactions = res.result.reactions;
 
@@ -41,26 +41,71 @@ const getFarcasterLikes = async (castHash) => {
     // Filter only 'like' items
     const likeItems = reactions.filter(item => item.type === 'like');
 
-    // check agains address here (callerAddress)
+    console.log('likeItems', likeItems)
 
-    // Extract required properties 
-    const filteredReactionData = likeItems.map(item => ({
-      hash: item.hash,
-      reactor: {
-        fid: item.reactor.fid,
-        username: item.reactor.username
-      }
-    }));
+    // Check if there is at least one like with a matching address
+    const hasMatchingAddress = await Promise.all(
+      likeItems.map(async item => {
+        try {
+          const fid = item.reactor.fid;
 
-    console.log({ filteredReactionData });
+          /// @dev uncomment for production
+          //const verificationResponse = await warpcaster(`/verifications?fid=${callerAddress}`);
+          /// @dev comment for production
+          const verificationResponse = await warpcaster(`/verifications?fid=3`);
+          const address = verificationResponse.result.verifications[0]?.address;
 
-    return filteredReactionData;
+          console.log({ address })
+          // Check if the address matches the callerAddress
+          return address === callerAddress;
+        } catch (verificationError) {
+          console.error('Error in verification request:', verificationError);
+          // Handle verification error, e.g., by returning false
+          return false;
+        }
+      })
+    );
+
+    // Return 1 if there is a match, otherwise return 0
+    return hasMatchingAddress.includes(true) ? 1 : 0;
   } catch (error) {
     console.error('Error in getFarcasterLikes:', error);
+    // Handle the main error, e.g., by returning 0 or rethrowing the error
+    return 0;
   }
 };
 
-// TODO handle input of args to the function
-getFarcasterLikes('0x92dc68040066319b2174c791f269af3ea5ac1bd1');
+/// @dev comment for production
+// ---------------------------------------------
+// Usage example
+// ---------------------------------------------
+const castHash = '0xcc1cb08481aa0b048588a59a608f27c7214ac79e'; // https://warpcast.com/nanditmehra/0x08049923
+const callerAddress = '0x8fc5d6afe572fefc4ec153587b63ce543f6fa2ea';
 
-return Functions.encodeUint256(1);
+getFarcasterLikes(castHash, callerAddress)
+  .then(result => {
+    // Handle the result (1 if there is a match, 0 otherwise)
+    console.log(result);
+  })
+  .catch(error => {
+    // Handle the error
+    console.error(error);
+  });
+// ---------------------------------------------
+
+/// @dev uncomment for production
+// ---------------------------------------------
+// production code
+// ---------------------------------------------
+// return Functions.encodeUint256(getFarcasterLikes(castHash, callerAddress)
+//   .then(result => {
+//     // Handle the result (1 if there is a match, 0 otherwise)
+//     console.log(result);
+//     return result
+//   })
+//   .catch(error => {
+//     // Handle the error
+//     console.error(error);
+//   }));
+// return Functions.encodeUint256(1);
+// ---------------------------------------------
